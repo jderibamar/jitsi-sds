4b097956a30ab6ed9dc840ebaf0348742b9bd998
jderibamar <jjrrdd@gmail.com>
2021-08-26 13:00:37 -0300

    up de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 
@@ -7,9 +9,7 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [ BrowserModule, AppRoutingModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
